a427e2cc87a7591c8eb4fe43b4a8c567706553d7
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2025-03-20 16:47:53 -0700

    Make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -62,15 +62,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     
     const productId = button.dataset.productId;
-
     let matchingItem;
-
     cart.forEach((item) => {
       if(productId === item.productId) {
         matchingItem = item;
       }
     });
-
     if(matchingItem) {
       matchingItem.quantity += 1;
     }
@@ -80,8 +77,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         quantity: 1
       });
     }
-    console.log(cart);
-  });
 
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  });
 });
 
