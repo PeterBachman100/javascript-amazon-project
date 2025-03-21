a3ed7ed0d9d71c1bd39707b714d39bd8ae0f4d3e
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2025-03-21 11:25:16 -0700

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = []
+
+export function addToCart(productId) {
+    let matchingItem;
+    const productQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+    cart.forEach((cartItem) => {
+      if(productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+    if(matchingItem) {
+      matchingItem.quantity += productQuantity;
+    }
+    else {
+      cart.push({
+        productId: productId,
+        quantity: productQuantity
+      });
+    }
+  }
