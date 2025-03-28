05fcf9c30f1122b474726568eaa458e659e68357
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2025-03-28 13:21:46 -0700

    Add 2 more test cases for formatCurrency

--- tests/utils/moneyTest.js
+++ tests/utils/moneyTest.js
@@ -13,4 +13,12 @@ describe('test suite: formatCurrency', () => {
         expect(formatCurrency(2000.5)).toEqual('20.01');
     });
 
+    it('rounds down to the nearest cent', () => {
+        expect(formatCurrency(2000.4)).toEqual('20.00');
+    });
+
+    it('works with negatives', () => {
+        expect(formatCurrency(-2095)).toEqual('-20.95');
+    });
+
 });
